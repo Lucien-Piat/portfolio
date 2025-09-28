8e4b451adbee5906802b97a7da902b63a4e74f94
Lpiat <lucienpiat33@gmail.com>
2025-09-28 22:42:20 +0200

    Add .gitignore and clean up repository

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 AUTHOR = 'Lucien Piat'
-SITENAME = 'Lucien Piat'
+SITENAME = 'Lucien Piat Portfolio'
 SITEURL = 'https://lucienpiat.fr'
 
-PATH = "content"
-
+PATH = 'content'
 TIMEZONE = 'Europe/Paris'
-
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
+# IMPORTANT: Set the theme path
+THEME = 'themes/under-construction'
+
+# Disable feed generation
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Disable unnecessary pages for under construction
 DIRECT_TEMPLATES = ['index']
 PAGINATED_TEMPLATES = {
     'index': None,
@@ -23,27 +28,14 @@ PAGINATED_TEMPLATES = {
     'author': None,
 }
 
-# Static files
-STATIC_PATHS = ['images', 'extra/CNAME']
+# Disable pagination
+DEFAULT_PAGINATION = False
+
+# Static paths
+STATIC_PATHS = ['extra/CNAME']
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
 }
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
-DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+# For local development
+RELATIVE_URLS = True
